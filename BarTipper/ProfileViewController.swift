62764daa85d86f7e531e7498a5d424a8cbb0718d
Levente Orban <orbanl@inf.u-szeged.hu>
2016-12-03 22:44:26 +0100

    Add the ProfileViewController.swift template

---
+++ BarTipper/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  BarTipper
+//
+//  Created by Levi on 2016. 12. 03..
+//  Copyright © 2016. levi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var name: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
